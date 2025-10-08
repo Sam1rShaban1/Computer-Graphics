f81c91b5d9bc9f8587a2bec28fa0daa3ac7837c2
Sam1rShaban1 <shabani.samir12@gmail.com>
2025-10-08 12:28:16 +0200

    Week3 Class Assignment

--- week3_class_assignment/main.js
+++ week3_class_assignment/main.js
@@ -1,9 +1,12 @@
 import * as THREE from 'three';
+import { emissive, metalness, roughness } from 'three/tsl';
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffffff);
+scene.background = new THREE.Color(0x00001f);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5
+camera.position.z = 10
+camera.position.y = 2
+camera.rotation.z = (Math.PI)
 
 const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(800, 600);
@@ -54,49 +57,53 @@ scene.add(directionalLight);
 const lightHelper = new THREE.DirectionalLightHelper(directionalLight)
 scene.add(lightHelper)
 
-const group = new THREE.Group()
-group.scale.x = .5
-group.scale.y = 1
-// group.position.y = .5
-group.rotation.x = Math.PI * .25
-scene.add(group)
+const ground = new THREE.PlaneGeometry(100,100)
+const groundMaterial = new THREE.MeshStandardMaterial({color: "lightgray", metalness: 0.7, roughness: 0.2})
+const plane = new THREE.Mesh( ground, groundMaterial) ;
+plane.rotation.x = -Math.PI / 2; // make it a floor
+plane.position.y = -1;  
+scene.add( plane );
+
+// const group = new THREE.Group()
+// group.scale.x = .5
+// group.scale.y = 1
+// // group.position.y = .5
+// group.rotation.x = Math.PI * .25
+// scene.add(group)
 
 const x = 0, y = 0;
-
-const ground = new THREE.PlaneGeometry(1000,1000)
-const groundMaterial = new THREE.MeshStandardMaterial({color: "orange", metalness: 0.7, roughness: 0.2})
-const plane = new THREE.Mesh( ground, groundMaterial) ;
-scene.add( plane );
 
 // const geometry = new THREE.SphereGeometry(1, 12, 25);
 const geometry = new THREE.CapsuleGeometry(1, 3, 20)
 // const material = new THREE.MeshBasicMaterial( { color: "red", wireframe:true } );
 // const material = new THREE.MeshLambertMaterial({color: "blue", wireframe:true});
-const material = new THREE.MeshStandardMaterial({color: "orange", metalness: 0.7, roughness: 0.2})
-const heart = new THREE.Mesh( geometry, material) ;
-heart.scale.set(1, 1, 1)
-scene.add( heart );
+const material = new THREE.MeshStandardMaterial({color: "purple", metalness: 0.7, roughness: 0.2})
+const capsule = new THREE.Mesh( geometry, material) ;
+capsule.rotateX(Math.PI / 2)
+capsule.rotateZ(Math.PI / 2)
 
-// const cylinder = new THREE.Mesh(
-//     new THREE.CylinderGeometry(1,1,1), 
-//     new THREE.MeshStandardMaterial({color: "blue"})
-// )
-// cylinder.position.x = 3
-// group.add(cylinder)
+scene.add( capsule );
 
-// const capsule = new THREE.Mesh(
-//     new THREE.CapsuleGeometry(1,1,1), 
-//     new THREE.MeshStandardMaterial({color: "green"})
-// )
-// capsule.position.x = -3
-// group.add(capsule)
+const cylinder = new THREE.Mesh(
+    new THREE.CylinderGeometry(1,1,1), 
+    new THREE.MeshStandardMaterial({color: "blue", metalness: 0.1, roughness:0.8})
+)
+cylinder.position.x = 5
+scene.add(cylinder)
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1,3,3), 
+    new THREE.MeshStandardMaterial({color: "red", metalness: 0.3, roughness: 0.4})
+)
+cube.position.x = -5
+scene.add(cube)
 
 
 function animate() {
     requestAnimationFrame(animate);
-    heart.rotation.x += 0.01;
-    heart.rotation.y += 0.01;
-    heart.rotation.z += 0.02;
+    // capsule.rotation.x += 0.01;
+    // capsule.rotation.y += 0.01;
+    capsule.rotation.z += 0.02;
     renderer.render(scene, camera);
 }
 
